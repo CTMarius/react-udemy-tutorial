524eac5038bb8d5c6062890cddb50d8a73773c0b
CTMarius <mt.cristea@gmail.com>
2023-03-21 09:32:27 +0100

    first

---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,11 @@
+export default function ExpenseItems() {
+  return (
+    <div>
+      <div>March 28th 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$298.65</div>
+      </div>
+    </div>
+  );
+}
