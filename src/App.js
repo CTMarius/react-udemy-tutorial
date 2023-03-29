672b6a9edbc64e8dfe3dc815e78258ab120c9460
CTMarius <mt.cristea@gmail.com>
2023-03-29 22:02:54 +0200

    Added ExpenseDate component and used it in ExpenseItems component. Also added css file for ExpenseDate component.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import ExpenseItems from "./components/ExpenseItems";
 import { expenses } from "./mocks/mocks";
 
 function App() {
-
   return (
     <div>
       <h2>Let's get started!</h2>
@@ -14,19 +13,19 @@ function App() {
       <ExpenseItems
         title={expenses[1].title}
         amount={expenses[1].amount}
-        date={expenses[1].date} 
+        date={expenses[1].date}
       />
       <ExpenseItems
-       title={expenses[2].title}
+        title={expenses[2].title}
         amount={expenses[2].amount}
-        date={expenses[2].date} 
+        date={expenses[2].date}
       />
       <ExpenseItems
         title={expenses[3].title}
         amount={expenses[3].amount}
         date={expenses[3].date}
-       />
-      </div>
+      />
+    </div>
   );
 }
 
--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,9 +1,10 @@
 import "./ExpenseItems.css";
+import ExpenseDate from "./ExpenseDate";
 
 export default function ExpenseItems(props) {
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-items__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
