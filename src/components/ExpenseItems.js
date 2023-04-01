fa2012d38bae6ce5c703f1d6ac092589cc286c96
CTMarius <mt.cristea@gmail.com>
2023-04-01 22:35:58 +0200

    Added Card component and used it in Expenses component

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,14 +1,15 @@
 import "./ExpenseItems.css";
+import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 
 export default function ExpenseItems(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.expenseData.date} />
       <div className="expense-items__description">
         <h2>{props.expenseData.title}</h2>
         <div className="expense-item__price">${props.expenseData.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,13 +1,14 @@
 import "./Expenses.css";
 import ExpenseItems from "./ExpenseItems";
+import Card from "./Card";
 
 export default function Expenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItems expenseData={props.expenses[0]} />
       <ExpenseItems expenseData={props.expenses[1]} />
       <ExpenseItems expenseData={props.expenses[2]} />
       <ExpenseItems expenseData={props.expenses[3]} />
-    </div>
+    </Card>
   );
 }
